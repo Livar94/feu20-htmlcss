03ba240810be1d639076f2e696dda36f1737e0b3
livar94 <Hans.Mattin-Lassei@epndata.com>
2021-04-05 14:09:38 +0200

    ecommerce

---
+++ zzz-ecommerce-vue/shop/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <navbar />
+
+    <div class="container mt-5">
+      <router-view/>
+    </div>
+    
+  </div>
+</template>
+
+<script>
+import Navbar from './components/Navigation/Navbar'
+export default {
+  components: {
+    Navbar
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ zzz-ecommerce-vue/shop/src/views/Products.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <product-card-deck />
+  </div>
+</template>
+
+<script>
+import { mapActions } from 'vuex'
+import ProductCardDeck from '../components/Products/ProductCardDeck.vue'
+export default {
+  components: {
+    ProductCardDeck
+  },
+  methods: {
+    ...mapActions(['getProducts'])
+  },
+  created() {
+    this.getProducts()
+  }
+}
+</script>
+
+
+<style>
+</style>
